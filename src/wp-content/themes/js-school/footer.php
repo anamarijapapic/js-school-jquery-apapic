1f9c43359e3a0cd518e0568dc6947496ee1a8ac8
Anamarija Papic <anamarija.papic@agilo.co>
2022-11-23 15:10:57 +0100

    Remove unnecessary files from the _s theme

--- src/wp-content/themes/js-school/footer.php
+++ src/wp-content/themes/js-school/footer.php
@@ -11,23 +11,6 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'js-school' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'js-school' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'js-school' ), 'js-school', '<a href="https://agilo.co">Anamarija Papić, Agilo</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
 
 </body>
