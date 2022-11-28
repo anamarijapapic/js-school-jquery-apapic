15cff2c53d170b8afeeac0675d6948e9e22fe16d
Anamarija Papic <anamarija.papic@agilo.co>
2022-11-28 14:00:46 +0100

    Remove assets folder which should be ignored

--- src/wp-content/themes/js-school-wp/assets/js/main.js
+++
@@ -1,2 +0,0 @@
-!function(o){"use strict";function e(o){return o&&"object"==typeof o&&"default"in o?o:{default:o}}var t=e(window.jQuery);console.log(1+2),console.log(t.default(".something"))}();
-//# sourceMappingURL=main.js.map
