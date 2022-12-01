47d047cece34258106040eef1d46645ab3c6dac1
Anamarija Papic <anamarija.papic@agilo.co>
2022-12-01 14:20:10 +0100

    Stackoverflow tags: Implement AJAX request cache

--- src/html/js/components/archive-stackoverflow-post.js
+++ src/html/js/components/archive-stackoverflow-post.js
@@ -1,13 +1,31 @@
 import $ from 'jquery';
 
+var localCache = {
+    data: {},
+    remove: function ( tag_name ) {
+        delete localCache.data[ tag_name ];
+    },
+    exist: function ( tag_name ) {
+        return localCache.data.hasOwnProperty( tag_name ) && localCache.data[ tag_name ] !== null;
+    },
+    get: function ( tag_name ) {
+        // console.log( 'Getting cache for tag: ' +  tag_name );
+        return localCache.data[ tag_name ];
+    },
+    set: function ( tag_name, cachedData ) {
+        localCache.remove( tag_name );
+        localCache.data[ tag_name ] = cachedData;
+    }
+};
+
 $( function() {
     $( '[data-bs-toggle="popover"]' ).on( 'mouseenter', function ( event ) {
         var el = $( this );
-
-        var tag_name = el.text().trim();
-
+        
         // disable this event after first binding 
-        el.off( event );
+        // el.off( event );
+        
+        var tag_name = el.text().trim();
 
         $.ajax( {
             url: window.ajaxurl, 
@@ -16,6 +34,16 @@ $( function() {
                 tag_name: tag_name,
             },
             dataType: 'json',
+            cache: true,
+            beforeSend: function () {
+                if ( localCache.exist( tag_name ) ) {
+                    // localCache.get( tag_name );
+                    return false;
+                }
+                // reserve local cache data key so that no more ajax requests are made
+                localCache.set( tag_name, undefined );
+                return true;
+            },
             success: function( data ) {
 
                 el.popover( {
@@ -37,6 +65,9 @@ $( function() {
             },
             error: function( error ) { 
                 // console.log( error ) 
+            },
+            complete: function ( jqXHR, textStatus ) {
+                localCache.set( tag_name, jqXHR );
             },
         } );
     });
