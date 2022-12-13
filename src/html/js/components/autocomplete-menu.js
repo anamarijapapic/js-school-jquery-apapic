e47ea859d2851a64c1e17eb9cbbcc8d11467d27d
Anamarija Papic <anamarija.papic@agilo.co>
2022-12-13 15:38:54 +0100

    Autocomplete: replace vars, sort in WP Query

--- src/html/js/components/autocomplete-menu.js
+++ src/html/js/components/autocomplete-menu.js
@@ -1,60 +1,57 @@
 import $ from 'jquery';
 
-$( function() {
+let globalTimeout = null;
 
-    var globalTimeout = null;
+let currentXhr;
 
-    var currentXhr;
+$( '#list' ).on( 'keyup', function() {
+    
+    currentXhr && currentXhr.readyState != 4 && currentXhr.abort(); // clear previous request
 
-    $( "#list" ).on( 'keyup', function() {
+    let value = $( this ).val();
+    
+    if ( value.length > 1 ) {
         
-        currentXhr && currentXhr.readyState != 4 && currentXhr.abort(); // clear previous request
-
-        var value = $( this ).val();
-        
-        if ( value.length > 1 ) {
+        if ( globalTimeout != null ) {
+            clearTimeout( globalTimeout );
+        }
+        globalTimeout = setTimeout( function() {
+            globalTimeout = null;
             
-            if ( globalTimeout != null ) {
-                clearTimeout( globalTimeout );
-            }
-            globalTimeout = setTimeout( function() {
-                globalTimeout = null;
-                
-                //ajax code
-                currentXhr = $.ajax({
-                    url: 'https://js-school-wp.test/wp-admin/admin-ajax.php',
-                    data: {
-                        action: 'js_school_wp_load_autocomplete',
-                        keyword: value,
-                        blog_id: $( '#blog-info' ).data( 'blogid' ),
-                    },
-                    beforeSend: function() {
-                        $( '#autocomplete-menu' ).show();
-                        $( '#loadingAnimation' ).removeClass( 'd-none' );
-                        $( '#loadingAnimation' ).addClass( 'd-block' );
-                    },
-                    success: function( data ) {
-                        // console.log( data.data );
-                        $( '#autocomplete-menu > .list-group > .list-group-item' ).not( '#loadingAnimation' ).remove();
-                        $( '#autocomplete-menu > .list-group' ).append( data.html );
-                        $( '#loadingAnimation' ).removeClass( 'd-block' );
-                        $( '#loadingAnimation' ).addClass( 'd-none' );
-                    },
-                    complete: function() {
-                        // ajax request completed
-                    }
-                });
+            //ajax code
+            currentXhr = $.ajax( {
+                url: 'https://js-school-wp.test/wp-admin/admin-ajax.php',
+                data: {
+                    action: 'js_school_wp_load_autocomplete',
+                    keyword: value,
+                    blog_id: $( '#blog-info' ).data( 'blogid' ),
+                },
+                beforeSend: function() {
+                    $( '#autocomplete-menu' ).show();
+                    $( '#loadingAnimation' ).removeClass( 'd-none' );
+                    $( '#loadingAnimation' ).addClass( 'd-block' );
+                },
+                success: function( data ) {
+                    // console.log( data.data );
+                    $( '#autocomplete-menu > .list-group > .list-group-item' ).not( '#loadingAnimation' ).remove();
+                    $( '#autocomplete-menu > .list-group' ).append( data.html );
+                    $( '#loadingAnimation' ).removeClass( 'd-block' );
+                    $( '#loadingAnimation' ).addClass( 'd-none' );
+                },
+                complete: function() {
+                    // ajax request completed
+                }
+            } );
 
-            }, 300 );  
-        }
-        else {
-            $( '#autocomplete-menu' ).hide();
-            $( '#autocomplete-menu > .list-group > .list-group-item' ).not( '#loadingAnimation' ).remove();
-            $( '#loadingAnimation' ).removeClass( 'd-block' );
-            $( '#loadingAnimation' ).addClass( 'd-none' );
-        }
+        }, 300 );  
+    }
+    else {
+        $( '#autocomplete-menu' ).hide();
+        $( '#autocomplete-menu > .list-group > .list-group-item' ).not( '#loadingAnimation' ).remove();
+        $( '#loadingAnimation' ).removeClass( 'd-block' );
+        $( '#loadingAnimation' ).addClass( 'd-none' );
+    }
 
-	} );
 } );
 
 window.selectOrganization = function( val ) {
--- src/wp-content/themes/js-school-wp/inc/organizations/extras.php
+++ src/wp-content/themes/js-school-wp/inc/organizations/extras.php
@@ -3,14 +3,14 @@
 define( 'JS_SCHOOL_WP_ORGANIZATIONS_PER_PAGE', 10 );
 
 function js_school_wp_handle_ajax_load_autocomplete() : void {
-    $page = (int) ( $_GET['js_school_wp_page'] ?? 1 );
     $blog_id = (int) ( $_GET['blog_id'] ?? 1 );
 
 	$query = new WP_Query( [
 		'post_type'      => 'organization',
-		'posts_per_page' => JS_SCHOOL_WP_ORGANIZATIONS_PER_PAGE,
-		'paged'          => $page,
+		'posts_per_page' => -1,
 		'post_status'    => 'publish',
+		'orderby' 		 => 'post_title',
+		'order'			 => 'ASC',
         's'              => $_GET['keyword'] ?? null,
 	] );
 
@@ -25,15 +25,11 @@ function js_school_wp_handle_ajax_load_autocomplete() : void {
 				'date_published' => $post->post_date,
 				'date_modified'  => $post->post_modified,
 			] );
+
+			$organization_name = get_the_title( $post->ID );
+			$escaped_organization_name = json_encode( $organization_name );
+			$html .= "<button type='button' class='list-group-item list-group-item-action' onclick='selectOrganization($escaped_organization_name);'>{$organization_name}</button>";
 		}
-	}
-
-	usort( $organizations_json, function( $a, $b ) { return strnatcasecmp( $a['name'], $b['name'] ); } );
-
-	foreach ( $organizations_json as $post ) {
-		$organization_name = get_the_title( $post['id'] );
-		$escaped_organization_name = json_encode( $organization_name );
-		$html .= "<button type='button' class='list-group-item list-group-item-action' onclick='selectOrganization($escaped_organization_name);'>{$organization_name}</button>";
 	}
 
 	if ( $blog_id === JS_SCHOOL_WP_2_SUBSITE_ID ) {
@@ -43,10 +39,8 @@ function js_school_wp_handle_ajax_load_autocomplete() : void {
 
 	wp_send_json( [
 		'success'     => true,
-		'page'        => $page,
 		'post_count'  => $query->post_count,
 		'found_posts' => $query->found_posts,
-		'max_page' 	  => $query->max_num_pages,
 		'html'    	  => $html,
 		'data'		  => $organizations_json,
 		'blog_id'	  => $blog_id,
